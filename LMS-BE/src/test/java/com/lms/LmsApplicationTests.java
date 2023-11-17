d0f21fd0226f90f5d922a06734314837adc77d13
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-11-17 15:36:36 +0530

    Update LmsApplicationTests.java

--- LMS-BE/src/test/java/com/lms/LmsApplicationTests.java
+++ LMS-BE/src/test/java/com/lms/LmsApplicationTests.java
@@ -1,13 +1,18 @@
-package com.lms;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = LmsApplicationTests.TestConfig.class)   
 class LmsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Configuration
+    static class TestConfig {
+        // Define any configuration beans or settings needed for testing
+    }
+    @Test
+    void contextLoads() {
+        // Your test logic here
+    }
 }

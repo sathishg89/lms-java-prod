5ea60b7ec1e2e9985874e41d0656342ab3b07ac7
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-12-10 11:24:46 +0530

    Update LmsApplication.java

--- LMS-BE/src/main/java/com/lms/LmsApplication.java
+++ LMS-BE/src/main/java/com/lms/LmsApplication.java
@@ -1,12 +1,9 @@
 package com.lms;
 
-import com.lms.config.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(DatabaseProperties.class)
 public class LmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(LmsApplication.class, args);

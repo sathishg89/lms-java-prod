3e0d43c3d9a75b4c655268f716dc7465d1884fb6
desetti praneeth <praneethdesetti@gmail.com>
2024-01-03 16:18:39 +0530

    hash change

--- lms-team-1-praneeth/LMS-FE/src/App.js
+++ lms-team-1-praneeth/LMS-FE/src/App.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './Components/Home/Home';
-import Login from './Components/Login/Login';
-import Forgot from './Components/Forgot';
-import LearnerDashboard from './Components/LearnerDashboard/LearnerDashboard';
-import Profile from './Components/Profile/Profile';
-import CourseDashboard from './Components/CourseDashboard/CourseDashboard';
-import AdminDashboard from './Components/AdminDashboard/AdminDashboard';
-import MainModule from './Components/Modules/MainModule';
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Forgot from "./Components/Forgot";
+import LearnerDashboard from "./Components/LearnerDashboard/LearnerDashboard";
+import Profile from "./Components/Profile/Profile";
+import CourseDashboard from "./Components/CourseDashboard/CourseDashboard";
+import AdminDashboard from "./Components/AdminDashboard/AdminDashboard";
+import MainModule from "./Components/Modules/MainModule";
 
 function App() {
   return (
-
     <div className="App">
-      <Router>
+     
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgotpassword" element={<Forgot />} />
           <Route path="/learnerdashboard" element={<LearnerDashboard />} />
           <Route path="/myprofile" element={<Profile />} />
-          <Route path="/coursedashboard/:courseName/:trainerName" element={<CourseDashboard />} />
+          <Route
+            path="/coursedashboard/:courseName/:trainerName"
+            element={<CourseDashboard />}
+          />
           <Route path="/admindashboard" element={<AdminDashboard />} />
           <Route path="/mainmodule" element={<MainModule />}></Route>
         </Routes>
-      </Router>
+    
     </div>
   );
 }

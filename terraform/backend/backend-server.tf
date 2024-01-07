c333655ef1bc46d71b593ecb91ca767172b7b6c1
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-07 18:14:13 +0530

    Update backend-server.tf

--- terraform/backend/backend-server.tf
+++ terraform/backend/backend-server.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "backend" {
   }
 
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
-  subnet_id             = "subnet-acdf850"
+  subnet_id             = "subnet-02ced4604c2c029c0"     # you will get this subnet-id when subnet created with terraform
 
   tags = {
     Name = "lms-backend"

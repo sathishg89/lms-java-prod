2174cd14c21b052c548f465267a2d4af70db991f
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-07 17:01:52 +0530

    Update frontend-output.tf

--- terraform/frontend/frontend-output.tf
+++ terraform/frontend/frontend-output.tf
@@ -1,8 +1,16 @@
 # frontend server output file
+
 output "frontend_public_ip" {
   value = aws_instance.frontend.public_ip
 }
 
 output "frontend_public_dns" {
   value = aws_instance.frontend.public_dns
+}
+
+
+# predefined username, while we giving ami id in server configuration we will know this
+
+output "backend_server_username" {
+  value = "ubuntu"  # Replace with your instance username if different
 }

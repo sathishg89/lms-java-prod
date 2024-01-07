2e92fb52423c4659018b54f51d4742f7927b326d
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-07 19:10:04 +0530

    Update Home.jsx

--- lms-fe/lmsv1/src/Components/Home.jsx
+++ lms-fe/lmsv1/src/Components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
   const handleButtonClick = async () => {
     try {
-      const response = await axios.get("http://54.215.194.205:8080/user/login");
+      const response = await axios.get("http://54.241.42.254:8080/user/login");
       console.log(response);
       setMessage(response.data);
     } catch (error) {

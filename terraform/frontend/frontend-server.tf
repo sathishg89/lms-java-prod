dc8906fcddde2fe8d4543e2608e9a4ba7596ec79
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-07 18:39:21 +0530

    Update frontend-server.tf

--- terraform/frontend/frontend-server.tf
+++ terraform/frontend/frontend-server.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-0da7657fe73215c0c"  # Replace with your desired AMI ID, this ami-id: ubuntu-20.04
+  ami           = "ami-0da7657fe73215c0c"       # Replace with your desired AMI ID, this ami-id: ubuntu-20.04
   instance_type = "t2.medium"
-  key_name      = "unv-california"  # Replace with your key pair name
+  key_name      = "unv-california"         # Replace with your key pair name
 
   root_block_device {
     volume_type           = "gp2"
@@ -10,27 +10,11 @@ resource "aws_instance" "frontend" {
   }
 
   vpc_security_group_ids = [aws_security_group.frontend_sg.id]
-  subnet_id             = "subnet-02ced4604c2c029c0"             # Replace with your subnet ID, you will get while creating the Subnet using Terraform
+  subnet_id             = "subnet-02ced4604c2c029c0"         # Replace with your subnet ID, you will get while creating the Subnet using Terraform
 
   tags = {
     Name = "lms-frontend"
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update -y
-              sudo apt-get install -y curl git
-
-              sudo curl -fsSL https://get.docker.com -o get-docker.sh
-              sudo sh get-docker.sh
-              sudo usermod -aG docker ubuntu  # Add ubuntu user to docker group
-
-              sudo git clone -b terraform https://github.com/muralialakuntla3/lms-java.git /home/ubuntu/lms-java
-              sudo chown -R ubuntu:ubuntu /home/ubuntu/lms-java  # Change ownership to ubuntu user
-
-              sudo docker network create -d bridge lmsnetwork
-              sudo docker build -t muralialakuntla3/terraform-fe /home/ubuntu/lms-java/lms-fe/lmsv1
-              sudo docker run -d --name fe --network lmsnetwork -p 80:80 muralialakuntla3/terraform-fe
-              echo "Frontend Docker deployment completed" > /var/log/user-data.log 2>&1
-              EOF
+  user_data = file("${path.module}/frontend.sh")
 }

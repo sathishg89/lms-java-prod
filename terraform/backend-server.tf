fd84f4eaf1431332a7f5be525ddef5e90021bb30
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-07 13:18:46 +0530

    Update and rename docker-server.tf to backend-server.tf

--- terraform/backend-server.tf
+++ terraform/backend-server.tf
@@ -38,23 +38,3 @@ resource "aws_instance" "my_ec2_instance" {
   }
 }
 
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all_traffic"
-  description = "Allow all inbound and outbound traffic"
-
-  vpc_id = aws_vpc.my_vpc.id  # Replace with the ID of your VPC
-
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}

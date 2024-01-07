47dcaa5b68f645f004dfbfca1096fd359f99bb4b
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-07 17:09:24 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
 # this file is used to print vpc-id and subnet-id, we will use them in firewall and server file
 
 output "vpc_id" {
-  value = aws_vpc.my_vpc.id
+  value = aws_vpc.lms_vpc.id
 }
 
 output "subnet_id" {
-  value = aws_subnet.public_subnet.id
+  value = aws_subnet.lms_public_subnet.id
 }

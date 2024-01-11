46a5e695427949c63163c2289cec22fd5cec07ff
adhanduprolu <127104799+adhanduprolu@users.noreply.github.com>
2024-01-11 15:48:41 +0530

    Update Home.jsx

--- lms-fe/lmsv1/src/Components/Home.jsx
+++ lms-fe/lmsv1/src/Components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
   const handleButtonClick = async () => {
     try {
-      const response = await axios.get("http://54.241.42.254:8080/user/login");
+      const response = await axios.get("http://52.14.205.64:8080/user/login");
       console.log(response);
       setMessage(response.data);
     } catch (error) {
